54702da92f0e4bf6de21d5e47df57c0110342744
Rsiu95 <rsiu95@gmail.com>
2023-09-20 21:45:03 +1000

    update contact form
    Started contact form design

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import smtplib, os
 from dotenv import load_dotenv
 from flask_bootstrap import Bootstrap5
+
 
 load_dotenv()
 
@@ -13,9 +14,31 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 Bootstrap5(app)
 
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def home():
+    
+    if request.method == "POST":
+        data = request.form
+        print(f"Name: {data['name']}\nEmail: {data['email']}\nPhone: {data['phone']}\nMessage: {data['message']}")
+        try:
+            connection = smtplib.SMTP("smtp.gmail.com", port=587)
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=MY_EMAIL,
+                msg=f"Subject: {data['name']} has contacted you! \n\nName: {data['name']}\nEmail: {data['email']}\nPhone: {data['phone']}\nMessage: {data['message']}"
+            )
+            connection.quit()
+            return jsonify({"success": True})
+        except Exception as e:
+            return jsonify({"success": False, "error_message": str(e)})
     return render_template("index.html")
+
+@app.route("/about-me")
+def about_me():
+    return render_template("about-me.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -19,3 +19,32 @@ function enableSmoothScrolling() {
 document.addEventListener('DOMContentLoaded', function() {
     enableSmoothScrolling();
 });
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("contactForm");
+    const successMessage = document.getElementById("successMessage");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        const formData = new FormData(form);
+
+        fetch("/", {
+            method: "POST",
+            body: formData,
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.success) {
+                    successMessage.style.display = "block";
+                    form.reset();
+                } else {
+                    console.error("Error:", data.error_message);
+                }
+            })
+            .catch((error) => {
+                console.error("Fetch Error:", error);
+            });
+    });
+});
